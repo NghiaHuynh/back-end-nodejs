3b84e244d074b63f23fabbdd84d2f9bf9332b9d4
NghiaHuynh <hhnghiait08@gmail.com>
2024-10-24 23:38:16 +0700

    Viết API Thêm, Sửa, Xóa Người Dùng

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser"; // dùng cái này để lấy id trong ur
 import viewEngine from "./config/viewEngine.js";
 import initWebRoute from "./route/web.js";
 import { connectDB } from "./config/connectDB.js";
+// import cors from "cors";
 
 import path from "path";
 import dotenv from 'dotenv';
@@ -14,9 +15,30 @@ dotenv.config();
 let app = express();
 
 //config app
+// app.use(cors());
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoute(app);
@@ -26,8 +48,8 @@ connectDB();
 //If port === undefined => = 9090
 let port = process.env.PORT || 9090;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     // khi lắng nghe thành công thì callback function run
     //callback funtion
-    console.log("Backend is runing on port : "+ port)
+    console.log("Backend is runing on port : " + port)
 });
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
+import e from 'express';
 import db from '../models/index';
+import { raw } from 'body-parser';
 
 
 let handleUserLogin = async (email, password) => {
@@ -8,8 +10,7 @@ let handleUserLogin = async (email, password) => {
     if (isExist) {
         
         let user = await db.User.findOne({
-            where: {email: email},
-            raw: true
+            where: {email: email}
         })
         if (user) {
             let compareResult = await comparePassword(password, user.password);
@@ -45,7 +46,91 @@ let comparePassword = async (password, userPassword) => {
     return await bcrypt.compare(password, userPassword);
 }
 
+let getAllUsers = async (id) => {
+    if (id.toLowerCase() === 'all') {
+        return await db.User.findAll({
+            attributes: {
+                exclude: ['password']
+            }
+        });
+    }
+    return await db.User.findOne({
+        where: {id: id},
+        attributes: {
+            exclude: ['password']
+        }
+    })
+}
+
+let createNewUser = async (data) => {
+    let checkEmail = await checkUserEmailExist(data.email);
+    if (checkEmail) {
+        return {
+            errCode: 1,
+            message: 'Email already exists'
+        }
+    }
+    data.password = hashPassword(data.password);
+    return await db.User.create(data);
+}
+
+let updateUserData = async (data) => {
+    //raw: false: only return data
+    //raw: true: return instant object data
+    let user = await db.User.findOne({where: {id: data.id},
+        raw : false
+    });
+    if (!user){
+        return {
+            errCode: 2,
+            message: 'User does not exist'
+        }
+    }else{
+        user.firstName = data.firstName;
+        user.lastName = data.lastName;
+        user.address = data.address;
+        user.phoneNumber = data.phoneNumber;
+        if (data.password) {
+            user.password = hashPassword(data.password);
+        }
+    }
+    await user.save();
+    return {
+        errCode: 0,
+        message: 'Update the user success!'
+    }
+}
+
+let deleteUserById = async (id) => {
+    if (!id) {
+        return {
+            errCode: 1,
+            message: 'Missing required parameters'
+        }
+    }else if (id.toLowerCase() === 'all') {
+        return await db.User.destroy({
+            where: {},
+            truncate: true
+        });
+    }else if (isNaN(id)) {
+        return {
+            errCode: 2,
+            message: 'Id must be a number'
+        }
+    }
+
+    return await db.User.destroy({where: {id: id}});
+}
+
+let hashPassword = (password) => {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+}
+
 export default {
     handleUserLogin: handleUserLogin,
-    checkUserEmailExist: checkUserEmailExist
+    checkUserEmailExist: checkUserEmailExist,
+    getAllUsers: getAllUsers,
+    createNewUser: createNewUser,
+    updateUserData: updateUserData,
+    deleteUserById: deleteUserById
 }
